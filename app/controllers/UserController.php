d8109814a675b719d80600be15d2a929a267bc12
Trần Thanh Tùng <thanhtung@vinades.vn>
2025-04-30 10:27:14 +0700

    start project

---
+++ app/controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+class UserController extends Controller {
+
+    public $data = [];
+    public $model = [];
+                   
+    public function __construct() {
+        
+    }
+                    
+    public function index() {
+                        
+    }
+}
